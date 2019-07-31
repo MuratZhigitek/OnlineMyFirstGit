4d48d142c1fe3ac2424372721a94487eb64bc090
MuratZhigitek <zhigitek@gmail.com>
2019-07-30 21:47:06 -0400

    Added selenuim script and modifies firstTest class

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -4,6 +4,7 @@ public class FirstTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World!git add .");
+        System.out.println("Testtouch .gitignore");
     }
 
 }
