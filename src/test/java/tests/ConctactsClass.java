d133ad567ee8d0765c3c2fa4c22dbe0b2969855f
MuratZhigitek <zhigitek@gmail.com>
2019-08-13 21:42:37 -0400

    modified

--- src/test/java/tests/ConctactsClass.java
+++ src/test/java/tests/ConctactsClass.java
@@ -6,6 +6,11 @@ public class ConctactsClass {
 
     @Test
     public void test1(){
+        System.out.println("driver open vytrck");
+        System.out.println("go to activities" +
+                "calendar");
+        System.out.println("testing" +
+                "allo git");
 
     }
 }
